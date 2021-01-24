fa6637320c465e7a81d810569231208913d05761
loreabad6 <lore.abad6@gmail.com>
2021-01-24 19:28:24 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ggraph-spatial-examples
+
+Showcasing the possibilities of using `ggraph` with a layout that understands geographical space positions via `sf`. These examples use `sfnetworks` and my implementation of `ggraph` still under construction before a PR is possible, and can be installed as:
+
+```r
+remotes::install_github("loreabad6/ggraph")
+```
+
+## Airports in the U.S.
+
+This example is inspired on this [wonderful blogpost on Network Visualization](https://kateto.net/sunbelt2019#overlaying-networks-on-geographic-maps), where I tried to recreate the last plot. 
+
+![](figs/us_airports.png)
+
+Code [here](code/airports.R).
---
+++ code/airports.R
@@ -0,0 +1,51 @@
+airports = read.csv("https://raw.githubusercontent.com/kateto/R-Network-Visualization-Workshop/master/Data%20files/Dataset3-Airlines-NODES.csv")
+flights = read.csv("https://raw.githubusercontent.com/kateto/R-Network-Visualization-Workshop/master/Data%20files/Dataset3-Airlines-EDGES.csv")
+library(sf)
+library(sfnetworks)
+library(tidygraph)
+nodes = st_as_sf(airports, coords = c("longitude", "latitude"), crs = 4326) %>%
+  mutate(ID = as.character(ID))
+edges = flights %>%
+  mutate(from = as.character(Source), to = as.character(Target))
+net = sfnetwork(nodes, edges, node_key = "ID") %>%
+  st_transform(2163)
+
+library(rnaturalearth)
+usa = ne_countries(scale = "medium", country = "United States of America", returnclass = "sf") %>%
+  st_cast("POLYGON") %>%
+  mutate(area = st_area(geometry)) %>%
+  filter(area == max(area)) %>%
+  st_transform(2163)
+
+# remotes::install_github("loreabad6/ggraph")
+library(ggplot2)
+library(ggraph)
+g = net %>%
+  filter(centrality_degree() > 10) %>%
+  ggraph(layout = 'sf') +
+  geom_sf(data = usa, color = NA, fill = "grey30") +
+  geom_edge_arc(aes(width = Freq, color = Freq), alpha = 0.7, strength = 0.2) + # alpha = stat(index)
+  geom_node_sf(aes(size = Visits), shape = 21, color = "white", fill = "orange") +
+  # geom_node_text(aes(label = Code), color = "white", fontface = "bold", repel = TRUE) +
+  scale_edge_color_gradient("Connection frequency", low = "orange red", high = "orange") +
+  scale_size("No. of visits", range = c(1, 5)) +
+  scale_edge_width("Connection frequency", range = c(0.1, 0.7)) +
+  scale_edge_alpha(guide = 'none') +
+  guides(
+    edge_color = guide_legend(override.aes = list(shape = NA)),
+    width = guide_legend(override.aes = list(shape = NA)),
+    size = guide_legend(order = 1)
+  ) +
+  labs(title = "Busiest Airports in the U.S.") +
+  theme(
+    text = element_text(color = 'white', size = 9),
+    panel.background = element_rect(fill = "grey10"),
+    plot.background = element_rect(fill = "grey10"),
+    legend.position = 'bottom',
+    legend.spacing = grid::unit(1, 'mm'),
+    legend.key = element_rect(fill = 'transparent'),
+    legend.background = element_rect(fill = 'transparent'),
+    legend.box = 'vertical', legend.direction = 'horizontal'
+  )
+ggsave(g, filename = "figs/us_airports.png", dpi = 300, device = "png", width = 10.75, height = 10, units = "cm")
+knitr::plot_crop("figs/us_airports.png")
